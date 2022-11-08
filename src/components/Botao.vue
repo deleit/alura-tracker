92cfdd038cb925e0bc95271e93b25edefdcbeb1c
Deivison Leite <dlmacedo9@gmail.com>
2022-11-08 16:34:01 -0300

    First commit

---
+++ src/components/Botao.vue
@@ -0,0 +1,35 @@
+<template>
+    <button class="button" :disabled="disabled" @click="clicado">
+        <span class="icon">
+            <i :class="icon"></i>
+        </span>
+        <span>{{ label }}</span>
+    </button>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'Botao',
+        emits: ['clicado'],
+        props: {
+            disabled: {
+                type: Boolean
+            },
+            label: {
+                type: String,
+                required: true
+            },
+            icon: {
+                type: String,
+                required: true
+            },
+        },
+        methods: {
+            clicado () : void {
+                this.$emit('clicado');
+            }
+        }
+    });
+</script>
---
+++ src/components/Temporizador.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="is-flex is-align-items-center is-justify-content-space-between">
+        <Cronometro :tempoEmSegundos="tempoEmSegundos" />
+        <Botao 
+            :disabled="cronometroRodando"
+            label="play"
+            icon="fas fa-play"
+            @clicado="iniciar"
+        />
+        <Botao 
+            :disabled="!cronometroRodando"
+            label="stop"
+            icon="fas fa-stop"
+            @clicado="finalizar"
+        />
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    import Cronometro from './Cronometro.vue';
+    import Botao from './Botao.vue';
+
+    export default defineComponent({
+        name: 'Temporizador',
+        emits: ['aoTemporizadorFinalizado'],
+        components: {
+            Cronometro,
+            Botao
+        },
+        data () {
+            return {
+                tempoEmSegundos: 0,
+                cronometro: 0,
+                cronometroRodando: false
+            }
+        },
+        methods: {
+            iniciar () {
+                this.cronometro = setInterval(() => {
+                    this.tempoEmSegundos++;
+                }, 1000);
+                this.cronometroRodando = true;
+            },
+            finalizar () {
+                clearInterval(this.cronometro);
+                this.cronometroRodando = false;
+                this.$emit('aoTemporizadorFinalizado', this.tempoEmSegundos);
+                this.tempoEmSegundos = 0;
+            }
+        }
+    });
+</script>
