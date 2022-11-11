5ae8e43065e8998aa478a11c3ac2c099cbf8195b
Deivison Leite de Macedo <deivisonmacedo@prefeitura.sp.gov.br>
2022-11-11 15:35:33 -0300

    Adição de gerenciamento de estados com vuex

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,13 +1,28 @@
 <template>
     <div class="box formulario">
         <div class="columns">
-            <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+            <div class="column is-5" role="form" aria-label="Formulário para criação de uma nova tarefa">
                 <input 
                     type="text" 
                     class="input" 
                     placeholder="Qual tarefa você deseja iniciar?"
                     v-model="descricao"
                 >
+            </div>
+
+            <div class="column is-3">
+                <div class="select">
+                    <select v-model="idProjeto">
+                        <option value="">Selecione o projeto</option>
+                        <option 
+                            v-for="projeto in projetos"
+                            :value="projeto.id"
+                            :key="projeto.id"    
+                        >
+                            {{ projeto.nome }}
+                        </option>
+                    </select>
+                </div>
             </div>
 
             <div class="column">
@@ -18,30 +33,41 @@
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue';
-    import Temporizador from './Temporizador.vue';
+import { key } from '@/store';
+import { computed } from '@vue/reactivity';
+import { defineComponent } from 'vue';
+import { useStore } from 'vuex';
+import Temporizador from './Temporizador.vue';
 
-    export default defineComponent({
-        name: 'Formulario',
-        emits: ['aoSalvarTarefa'],
-        components: {
-            Temporizador
-        },
-        data () {
-            return {
-                descricao: ''
-            }
-        },
-        methods: {
-            finalizarTarefa (tempoDecorrido: number) : void {
-                this.$emit('aoSalvarTarefa', {
-                    duracaoEmSegundos: tempoDecorrido,
-                    descricao: this.descricao
-                });
-                this.descricao = '';
-            }
+export default defineComponent({
+    name: 'Formulario',
+    emits: ['aoSalvarTarefa'],
+    components: {
+        Temporizador
+    },
+    data () {
+        return {
+            descricao: '',
+            idProjeto: '',
         }
-    })
+    },
+    methods: {
+        finalizarTarefa (tempoDecorrido: number) : void {
+            this.$emit('aoSalvarTarefa', {
+                duracaoEmSegundos: tempoDecorrido,
+                descricao: this.descricao,
+                projeto: this.projetos.find(proj => proj.id == this.idProjeto)
+            });
+            this.descricao = '';
+        }
+    },
+    setup () {
+        const store = useStore(key);
+        return {
+            projetos: computed(() => store.state.projetos)
+        }
+    }
+})
 </script>
 
 <style>
