db466ec9d5a8ba7d4998dfec54d6fc85e97d0e4f
Deivison Leite <dlmacedo9@gmail.com>
2022-11-11 16:14:56 -0300

    Adição da funcionalidade de editar projetos

--- src/store/index.ts
+++ src/store/index.ts
@@ -13,13 +13,17 @@ export const store = createStore<Estado>({
         projetos: []
     },
     mutations: {
-        'ADICIONA_PROJETO'(state, nomeDoProjeto: string) {
+        'ADICIONA_PROJETO' (state, nomeDoProjeto: string) {
             const projeto = {
                 id: new Date().toISOString(),
                 nome: nomeDoProjeto
             } as IProjeto
 
             state.projetos.push(projeto)
+        },
+        'ALTERA_PROJETO' (state, projeto: IProjeto) {
+            const index = state.projetos.findIndex(proj => proj.id == projeto.id);
+            state.projetos[index] = projeto;
         }
     }
 })
