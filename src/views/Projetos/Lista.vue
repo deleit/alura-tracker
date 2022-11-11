c01529d5ff74a822e65ec874fe6a9bd8f1269312
Deivison Leite <dlmacedo9@gmail.com>
2022-11-11 16:38:39 -0300

    Aninhamento de rotas

---
+++ src/views/Projetos/Lista.vue
@@ -0,0 +1,52 @@
+<template>
+    <section>
+        <router-link to="/projetos/novo" class="button">
+            <span class="icon is-small">
+                <i class="fas fa-plus"></i>
+            </span>
+            <span>Novo projeto</span>
+        </router-link>
+        <table class="table is-fullwidth">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="projeto in projetos" :key="projeto.id">
+                    <td>{{ projeto.id }}</td>
+                    <td>{{ projeto.nome }}</td>
+                    <td>
+                        <router-link :to="`/projetos/${projeto.id}`" class="button">
+                            <span class="icon is-small">
+                                <i class="fas fa-pencil-alt"></i>
+                            </span>
+                        </router-link>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useStore } from '@/store';
+import { computed } from '@vue/reactivity';
+import { RouterLink } from 'vue-router';
+
+export default defineComponent({
+    name: 'Lista',
+    components: {
+        RouterLink
+    }, 
+    setup () {
+        const store = useStore();
+        return {
+            projetos: computed(() => store.state.projetos)
+        }
+    }
+});
+</script>
